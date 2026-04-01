3b6bf24349da5954124b64737970d45ab4f95223
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-01 12:14:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express') /* importing express */
+const app = express() /* creating an express instance */
+const port = 3000 /* defing a port number */
+
+const posts = [  /* our array of blog */
+    {
+        titolo: "Alla scoperta di React",
+        contenuto: "React è una libreria JavaScript per costruire interfacce utente dinamiche...",
+        immagine: "/static/images/react-guide.svg",
+        tags: ["programmazione", "frontend", "javascript"]
+    },
+    {
+        titolo: "I segreti del CSS Moderno",
+        contenuto: "Grid e Flexbox hanno rivoluzionato il modo in cui progettiamo il layout web...",
+        immagine: "/static/images/css-moderno.svg",
+        tags: ["design", "web", "css"]
+    },
+    {
+        titolo: "Guida a Node.js",
+        contenuto: "Node.js permette di eseguire JavaScript lato server con alte prestazioni...",
+        immagine: "/static/images/nodejs-back.svg",
+        tags: ["backend", "javascript", "server"]
+    },
+    {
+        titolo: "Ottimizzazione SEO per Web App",
+        contenuto: "Migliorare la visibilità della tua applicazione sui motori di ricerca è fondamentale...",
+        immagine: "/static/images/seo-tips.svg",
+        tags: ["marketing", "seo", "web"]
+    },
+    {
+        titolo: "Introduzione a TypeScript",
+        contenuto: "Aggiungere i tipi a JavaScript rende il codice più robusto e facile da manutenere...",
+        immagine: "/static/images/typescript-intro.svg",
+        tags: ["typescript", "programmazione", "frontend"]
+    }
+];
+
+app.use('/static', express.static('public')) /* to read static files */
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog') /* sending a response */
+})
+
+/* index */
+
+app.get('/bacheca', (req, res) => {
+    res.json(posts) /* sending a json response of posts */
+})
+
+/* show */
+
+app.get('/bacheca/:id', (req, res) => {
+    res.send('Dettagli del post con id: ' + req.params.id) /* response of post details*/
+})
+
+/* store */
+app.post('/bacheca', (req, res) => {
+    res.send('Creazione di un nuovo post ') /* response of a post creation */
+})
+/* update */
+app.put('/bacheca/:id', (req,res) => {
+    res.send('Aggiornamento del post con id: ' +req.params.id) /* response of post update */
+})
+/* destroy */
+app.delete('/bacheca/:id', (req,res) => {
+    res.send('Cancellazione del post con id: ' + req.params.id) /* response of post deletion */
+})
+
+app.listen(port, () => {
+  console.log(`Server avviato su http://localhost:${port}`) /* starting the server */
+})
