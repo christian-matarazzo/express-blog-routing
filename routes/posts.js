12cb9269676411ce3a387b2c2503688044762be4
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-01 12:52:08 +0200

    add bonuses

--- routes/posts.js
+++ routes/posts.js
@@ -45,8 +45,18 @@ router.get('/', (req, res) => {
 /* show */
 
 router.get('/:id', (req, res) => {
-    res.send('Dettagli del post con id: ' + req.params.id) /* response of post details*/
+const id = parseInt(req.params.id)
+
+const post = posts.find((post,index) => index === id)
+
+if (post) {
+    res.json(post)
+} else {
+    res.status(404).json({ error: 'Post non trovato' }) /* response if the post is not found */
+}
 })
+
+
 
 /* store */
 router.post('/', (req, res) => {
